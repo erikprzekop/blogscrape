15d314e941d4b1ff9d52091b2db6b1d92278c8a3
Erik Przekop <erikprzekop@Eriks-MacBook-Pro-2.local>
2015-11-11 15:30:40 -0500

    Mavenized it!

--- src/test/java/blogscrape/DeleteMeTest.java
+++ src/test/java/blogscrape/DeleteMeTest.java
@@ -1,14 +1,11 @@
 package blogscrape;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class DeleteMeTest {
 
 	@Test
 	public void test() {
-		fail("Remove this whole class");
 		DeleteMe deleteme = new DeleteMe();
 	}
 
