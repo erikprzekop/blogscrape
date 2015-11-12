5393394640f14ef6a3de67be67c7ebd438a10f18
Aziz Hararwala <ahararwala@Azizs-MacBook-Pro.local>
2015-11-12 14:38:19 -0500

    add capability to pass in second argument as comma-separated keyword list

--- src/main/java/blogscrape/BlogScraper.java
+++ src/main/java/blogscrape/BlogScraper.java
@@ -8,11 +8,25 @@ import org.jsoup.nodes.Document;
 
 public class BlogScraper {
 
+	private static final int URL_PARM_INDEX = 0;
+	private static final int KEYWORD_PARM_INDEX = 1;
+
 	public static void main(String[] args) throws Exception {
-		String urlParm = args[0];
+		String urlParm = args[URL_PARM_INDEX];
+		List<String> keywordsList = buildKeywordsFromArgs(args);
 		Document doc = Jsoup.connect(urlParm).get();
 		String body = doc.body().text();
-		System.out.println(new BlogScraper().scrape(body, Arrays.asList("Clean Code")));
+		System.out.println(new BlogScraper().scrape(body, keywordsList));
+	}
+
+	private static List<String> buildKeywordsFromArgs(String[] args) {
+		if (args.length == 2) {
+			String keywords = args[KEYWORD_PARM_INDEX];
+			if (keywords != null && !keywords.isEmpty()) {
+				return Arrays.asList(keywords.split(","));
+			}
+		}
+		return Arrays.asList("Clean Code");
 	}
 
 	public Boolean scrape(String bodyText, List<String> keywords) throws Exception {
