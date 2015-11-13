cbd938725d7290cc39ee5f7d4c35e7d886465291
William Gibbins <wgibbins@Williams-MacBook-Pro.local>
2015-11-13 13:42:29 -0500

    More scraping

--- src/main/java/blogscrape/ContactInfoMiner.java
+++ src/main/java/blogscrape/ContactInfoMiner.java
@@ -9,23 +9,56 @@ import org.jsoup.nodes.Document;
 public class ContactInfoMiner {
 
 	private DocumentProvider documentProvider;
+	private Pattern twitterRegexPattern;
+	private Pattern emailRegexPattern;
+	private Pattern facebookRegexPattern;
+	private Pattern linkedInRegexPattern;
 
 	public ContactInfoMiner(DocumentProvider provider) {
 		this.documentProvider = provider;
+		this.twitterRegexPattern = Pattern.compile("@([a-z0-9_]+)",Pattern.CASE_INSENSITIVE);
+		this.emailRegexPattern = Pattern.compile("([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9._-]+)",Pattern.CASE_INSENSITIVE);
+		this.facebookRegexPattern = Pattern.compile("(?:https?:\\/\\/)?(?:www\\.)?facebook\\.com\\/(?:(?:\\w)*#!\\/)?(?:pages\\/)?(?:[\\w\\-]*\\/)*([\\w\\-\\.]*)");
+		this.linkedInRegexPattern = Pattern.compile("(?:http?:\\/\\/)?(?:www\\.)?linkedin\\.com\\/(?:(?:\\w)*#!\\/)?(?:pages\\/)?(?:[\\w\\-]*\\/)*([\\w\\-\\.]*)");
 	}
 
 	public ContactInfo mine(ContactInfo contactInfo) throws IOException {
 		Document documentFromURL = documentProvider.getDocumentFromURL(contactInfo.getBlogAboutMeLink());
 		String bodyText = documentFromURL.body().text();
 		
-		scrapeTwitterHandle(contactInfo, bodyText);
+		attemptTwitterRegexScrape(contactInfo,twitterRegexPattern,bodyText);
+		attemptEmailRegexScrape(contactInfo,emailRegexPattern,bodyText);
+		attemptFacebookRegexScrape	(contactInfo,facebookRegexPattern,bodyText);
+		attemptLinkedInRegexScrape	(contactInfo,linkedInRegexPattern,bodyText);
+		
 		return contactInfo;
 	}
 
-	private void scrapeTwitterHandle(ContactInfo contactInfo, String bodyText) {
-		Matcher matcher = Pattern.compile("@([a-z0-9_]+)",Pattern.CASE_INSENSITIVE).matcher(bodyText);
+	private void attemptTwitterRegexScrape(ContactInfo contactInfo,Pattern regexPattern ,String bodyText) {
+		Matcher matcher = regexPattern.matcher(bodyText);
 		if(matcher.find()) {
 			contactInfo.setTwitterHandle(matcher.group(0));
+		}
+	}
+
+	private void attemptEmailRegexScrape(ContactInfo contactInfo,Pattern regexPattern ,String bodyText) {
+		Matcher matcher = regexPattern.matcher(bodyText);
+		if(matcher.find()) {
+			contactInfo.setEmailAddress(matcher.group(0));
+		}
+	}
+	
+	private void attemptFacebookRegexScrape(ContactInfo contactInfo,Pattern regexPattern ,String bodyText) {
+		Matcher matcher = regexPattern.matcher(bodyText);
+		if(matcher.find()) {
+			contactInfo.setFacebookAddress(matcher.group(0));
+		}
+	}
+	
+	private void attemptLinkedInRegexScrape(ContactInfo contactInfo,Pattern regexPattern ,String bodyText) {
+		Matcher matcher = regexPattern.matcher(bodyText);
+		if(matcher.find()) {
+			contactInfo.setLinkedIn(matcher.group(0));
 		}
 	}
 
--- src/test/java/blogscrape/ContactInfoMinerTest.java
+++ src/test/java/blogscrape/ContactInfoMinerTest.java
@@ -32,4 +32,49 @@ public class ContactInfoMinerTest {
 		assertEquals("@MrCleanCoder",result.getTwitterHandle());
 	}
 
+	@Test
+	public void testMineReturnsContactInfoWithEmailWhenFound() throws Exception {
+		String expectedBodyText = "<div class='Content'>Some random information, here is my email: clean.coder@gmail.com, you can send me any comment there.</div>";
+		ContactInfo info = new ContactInfo();
+		Document doc = new Document("");
+		doc.appendElement("body");
+		doc.body().text(expectedBodyText);
+		DocumentProvider mockDocumentProvider = Mockito.mock(DocumentProvider.class);
+		Mockito.when(mockDocumentProvider.getDocumentFromURL(Mockito.anyString())).thenReturn(doc);
+		
+		ContactInfoMiner miner = new ContactInfoMiner(mockDocumentProvider);
+		ContactInfo result = miner.mine(info);	
+		assertEquals("clean.coder@gmail.com",result.getEmailAddress());
+	}
+	
+	@Test
+	public void testMineReturnsContactInfoWithFacebookLinkWhenFound() throws Exception {
+		String expectedBodyText = "<div class='Content'>Some random information, find me on facebook! <a href='https://www.facebook.com/cleanest.coder'>, you can send me any comment there.</div>";
+		ContactInfo info = new ContactInfo();
+		Document doc = new Document("");
+		doc.appendElement("body");
+		doc.body().text(expectedBodyText);
+		DocumentProvider mockDocumentProvider = Mockito.mock(DocumentProvider.class);
+		Mockito.when(mockDocumentProvider.getDocumentFromURL(Mockito.anyString())).thenReturn(doc);
+		
+		ContactInfoMiner miner = new ContactInfoMiner(mockDocumentProvider);
+		ContactInfo result = miner.mine(info);	
+		assertEquals("https://www.facebook.com/cleanest.coder",result.getFacebookAddress());
+	}
+	
+	@Test
+	public void testMineReturnsContactInfoWithLinkedInLinkWhenFound() throws Exception {
+		String expectedBodyText = "<div class='Content'>Some random information, find me on linkedIn! <a href='http://www.linkedin.com/in/cleanestcoder'>, you can send me any comment there.</div>";
+		ContactInfo info = new ContactInfo();
+		Document doc = new Document("");
+		doc.appendElement("body");
+		doc.body().text(expectedBodyText);
+		DocumentProvider mockDocumentProvider = Mockito.mock(DocumentProvider.class);
+		Mockito.when(mockDocumentProvider.getDocumentFromURL(Mockito.anyString())).thenReturn(doc);
+		
+		ContactInfoMiner miner = new ContactInfoMiner(mockDocumentProvider);
+		ContactInfo result = miner.mine(info);	
+		assertEquals("http://www.linkedin.com/in/cleanestcoder",result.getLinkedIn());
+	}
+
 }
